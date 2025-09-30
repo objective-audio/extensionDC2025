5ade851785c8af310327c3b8d51b4111b965460f
Yuki Yasoshima <yukiyasos@gmail.com>
2025-09-30 15:15:24 +0900

    Remove isFinished state to allow testing post-finish behavior

--- extensionDC2025Sample.swiftpm/Samples/AsyncStream/AsyncStreamSampleView.swift
+++ extensionDC2025Sample.swiftpm/Samples/AsyncStream/AsyncStreamSampleView.swift
@@ -3,7 +3,6 @@ import SwiftUI
 struct AsyncStreamSampleView: View {
     let sample = AsyncStreamSample()
     @State private var currentValue: Int = 0
-    @State private var isFinished = false
     
     var body: some View {
         VStack(spacing: 20) {
@@ -13,24 +12,14 @@ struct AsyncStreamSampleView: View {
                 currentValue += 1
                 sample.send(currentValue)
             }
-            .disabled(isFinished)
             
             Button("Send +10") {
                 currentValue += 10
                 sample.send(currentValue)
             }
-            .disabled(isFinished)
             
             Button("Finish Stream") {
                 sample.finish()
-                isFinished = true
-            }
-            .disabled(isFinished)
-            
-            if isFinished {
-                Text("Stream finished - sending after finish won't be received")
-                    .font(.caption)
-                    .foregroundColor(.red)
             }
         }
         .navigationTitle("AsyncStream")
