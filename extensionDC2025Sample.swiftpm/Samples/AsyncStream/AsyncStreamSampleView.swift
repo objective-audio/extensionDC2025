7b95b84c6f42e5bb0dbfc04dbf80ccb3faf62d2b
Yuki Yasoshima <yukiyasos@gmail.com>
2025-09-30 18:23:41 +0900

    Set currentValue initial to 1 and move increment to end of button actions

--- extensionDC2025Sample.swiftpm/Samples/AsyncStream/AsyncStreamSampleView.swift
+++ extensionDC2025Sample.swiftpm/Samples/AsyncStream/AsyncStreamSampleView.swift
@@ -2,27 +2,26 @@ import SwiftUI
 
 struct AsyncStreamSampleView: View {
     let sample = AsyncStreamSample()
-    @State private var currentValue: Int = 0
+    @State private var currentValue: Int = 1
     
     var body: some View {
         VStack(spacing: 20) {
             Text("Current Value: \(currentValue)")
             
             Button("Send +1") {
-                currentValue += 1
                 sample.send([currentValue])
+                currentValue += 1
             }
             
             Button("Send +10") {
-                currentValue += 10
                 sample.send([currentValue])
+                currentValue += 10
             }
             
             Button("Send Batch (10 values)") {
-                currentValue += 1
                 let batchValues = Array(currentValue..<(currentValue + 10))
                 sample.send(batchValues)
-                currentValue += 9
+                currentValue += 10
             }
             
             Button("Finish Stream") {
